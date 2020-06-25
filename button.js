2e4401e8e3cc3064b532eb459ef6fb2892a86391
Nikolas Santis <nikosantis@gmail.com>
2020-06-25 12:27:42 -0400

    message status ok

--- button.js
+++ button.js
@@ -1,8 +1,47 @@
 const button = document.querySelector('.wrapper')
 const popUp = document.querySelector('#pop-up-message')
 const container = document.querySelector('.form-container')
-const cancel = document.querySelector('.form-header-button')
+const cancel = document.querySelector('#buttonClose')
+const cancelResponse = document.querySelector('#msgClose')
 const formWindow = document.querySelector('.form-window')
+const form = document.querySelector('#form-submit')
+const formMsg = document.querySelector('#formMsg')
+
+let statusForm = {
+  enviado: false,
+  loading: false
+}
+
+form.onsubmit = function(event) {
+  event.preventDefault()
+  statusForm = {
+    ...statusForm,
+    loading: true
+  }
+  setTimeout(() => {
+    statusForm = {
+      ...statusForm,
+      loading: false,
+      enviado: true
+    }
+    form.style.display = 'none'
+    formMsg.removeAttribute('style')
+    formMsg.onclick = function() {
+      popUp.classList.remove('oculto')
+      formWindow.classList.add('zero-height')
+      container.classList.remove('mostrar')
+      setTimeout(() => {
+        formWindow.classList.remove('zero-height')
+        button.classList.remove('expanded-height')
+        button.classList.add('collapsed-height')
+        container.style.display = 'none'
+        popUp.removeAttribute('style')
+        form.removeAttribute('style')
+        formMsg.style.display = 'none'
+      }, 500)
+    }
+  }, 1000)
+}
 
 popUp.onclick = function() {
   popUp.classList.add('oculto')
